a72bc08799016ac81094381b66b682a04bdbde35
Ilya Shenbin <ilya.shenbin@gmail.com>
2016-11-07 19:46:44 +0300

    Attention + ConvLayer with filters of different widths

--- attention.py
+++ attention.py
@@ -56,29 +56,37 @@ from keras.layers.wrappers import TimeDistributed, Bidirectional
 import tensorflow as tf
 tf.python.control_flow_ops = tf
 
+# inspired by https://arxiv.org/abs/1606.02601 and https://arxiv.org/abs/1508.06615
 inputs = Input(shape=(max_word_len, letters_count, ))
-lstm_fwd = LSTM(100, return_sequences=True)(inputs)
-lstm_bwd = LSTM(100, return_sequences=True, go_backwards=True)(inputs)
-blstm = merge([lstm_fwd, lstm_bwd], mode='concat')
 
-attention_dense = TimeDistributed(Dense(50, activation='tanh'))(blstm)
-attention_out = TimeDistributed(Dense(1, activation='linear'))(attention_dense)
+conv1d_list =  \
+[
+    Convolution1D(nb_filter=25 * filter_length, filter_length=filter_length, 
+                  border_mode='same', activation='relu',subsample_length=1)(inputs)
+    for filter_length in range(1, 8)
+]
+
+conv1d = merge(conv1d_list, mode='concat')
+
+#attention_rec = Bidirectional(LSTM(32, return_sequences=True))(conv1d)
+attention_hidden = TimeDistributed(Dense(50, activation='relu'))(conv1d)
+attention_out = TimeDistributed(Dense(1, activation='linear'))(attention_hidden)
 attention_softmax = Activation('softmax')(attention_out)
 
-attention_dot = merge([attention_softmax, blstm], mode='dot', dot_axes=(1,1))
+attention_dot = merge([attention_softmax, conv1d], mode='dot', dot_axes=(1,1))
 
 lambda_1 = Lambda(lambda x: x[:,0,:])(attention_dot)
 
 dense_1 = Dense(50, activation='sigmoid')(lambda_1)
 dense_2 = Dense(w2v_dim)(dense_1)
 
-lstm_att_model = Model(input=inputs, output=dense_2)
-lstm_att_model.summary()
-lstm_att_model.compile(loss='cosine_proximity', optimizer='adam', metrics=['cosine_proximity', 'mse'])
-lstm_att_model.fit(X, Y, batch_size=20, nb_epoch=1, validation_split=0)
+cnn_att_model = Model(input=inputs, output=dense_2)
+cnn_att_model.summary()
+cnn_att_model.compile(loss='cosine_proximity', optimizer='adam', metrics=['cosine_proximity', 'mse'])
+cnn_att_model.fit(X, Y, batch_size=20, nb_epoch=1, validation_split=0)
 
 
 
 word_to_pedict = [word_to_ohe_vector('апельсин')]
-b = lstm_att_model.predict(sequence.pad_sequences(word_to_pedict, maxlen=max_word_len))
-model.most_similar(positive=b, topn=20)
+b = cnn_att_model.predict(sequence.pad_sequences(word_to_pedict, maxlen=max_word_len))
+print(model.most_similar(positive=b, topn=20))
